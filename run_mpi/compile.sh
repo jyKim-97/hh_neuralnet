5dc6b8048d33693921ac6eb3fb47501d1f1cb836
JungyoungKim <jungyoung@MacBook-Pro.local>
2022-11-22 03:05:30 +0100

    compiler

---
+++ run_mpi/compile.sh
@@ -0,0 +1,8 @@
+INCDIR="../include/"
+
+mpicc -Wall -g -O3 -I../include/ -I${MKLROOT}/include \
+    -o main.out main.c \
+    ${INCDIR}/model.o ${INCDIR}/utils.o ${INCDIR}/build.o \
+    ${INCDIR}/rng.o ${INCDIR}/mt64.o ${INCDIR}/storage.o ${INCDIR}/measurement.o \
+    -lm
+#    -L${MKLROOT}/lib -lmkl_intel_lp64 -lmkl_core -lmkl_sequential
