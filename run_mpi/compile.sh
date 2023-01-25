01e9042f3aced43971d83027c7d1a3934a468231
JungyoungKim <jungyoung@MacBook-Pro-4.local>
2023-01-25 16:06:54 +0100

    delete old code

--- run_mpi/compile.sh
+++
@@ -1,9 +0,0 @@
-INCDIR="../include/"
-
-mpicc -o mpifor.o -c mpifor.c 
-mpicc -Wall -g -O3 -I../include/ -I${MKLROOT}/include \
-    -o main.out main.c \
-    ${INCDIR}/model.o ${INCDIR}/utils.o ${INCDIR}/build.o \
-    ${INCDIR}/rng.o ${INCDIR}/mt64.o ${INCDIR}/storage.o ${INCDIR}/measurement.o \
-    ./mpifor.o -lm
-#    -L${MKLROOT}/lib -lmkl_intel_lp64 -lmkl_core -lmkl_sequential
