e3b03eb1aa9a6fe4a0a26a28c4538f885e8ab483
jyKim-97 <rlawjddud73@khu.ac.kr>
2023-09-21 10:52:05 +0900

    add functions

--- include/hhtools.py
+++ include/hhtools.py
@@ -562,3 +562,120 @@ def load_summary(f):
     
     return obj
 
+
+
+# =============== Visualization ================== #
+def draw_quadratic_summary(data, fname=None, xl_raster=(1500, 2500), nsamples=20, wbin_t=1, fs=2000, shuffle=False):
+    
+    import hhsignal
+    from tqdm.notebook import tqdm
+    from scipy.ndimage import gaussian_filter1d
+    
+    teq = 0.5
+    
+    plt.figure(dpi=100, figsize=(9, 12))
+    plt.axes([0.1, 0.8, 0.8, 0.15])
+    
+    seq = np.arange(len(data["step_spk"]))
+    cr = [800, 1000, 1800, 2000]
+    cs = ["r", "b", "deeppink", "navy"]
+    
+    if shuffle:
+        np.random.shuffle(seq)
+        cr = None
+        cs = None
+        
+    
+    draw_spk(data["step_spk"], color_ranges=cr, colors=cs, xl=xl_raster, sequence=seq)
+    plt.ylabel("# neuron", fontsize=14)
+    plt.xlabel("Time (s)", fontsize=14)
+    xt, _ = plt.xticks()
+    plt.xticks(xt, labels=["%.3f"%(x/1000) for x in xt])
+    
+    title = "nid: %d"%(data["nid"][0])
+    for n in data["nid"][1:]:
+        title += ",%d"%(n)
+    plt.title(title, fontsize=14)
+
+    plt.twinx()
+    t = data["ts"] * 1e3
+    plt.plot(t, data["vlfp"][0], c='k', zorder=10, label=r"$V_T$")
+    plt.plot(t, data["vlfp"][1], c='b', lw=1, label=r"$V_F$")
+    plt.plot(t, data["vlfp"][2], c='r', lw=1, label=r"$V_S$")
+    plt.legend(fontsize=14, loc="upper left", ncol=3, edgecolor="none")
+    plt.ylabel("V", fontsize=14)
+    
+    # ----------------- Generate AC for x -----------------#
+    t0_set = np.random.uniform(low=teq, high=data["ts"][-1]-wbin_t, size=nsamples)
+    cc_set = [[] for _ in range(4)]
+    for t0 in tqdm(t0_set):
+        n0 = int(t0 * fs)
+        n1 = n0 + wbin_t * fs
+
+        for i in range(4):
+            if i < 3:
+                x = data["vlfp"][i][n0:n1]
+                y = x.copy()
+            else:
+                x = data["vlfp"][1][n0:n1]
+                y = data["vlfp"][2][n0:n1]
+
+            cc, tlag = hhsignal.get_correlation(x, y, fs, max_lag=0.1)
+            cc_set[i].append(cc)
+
+    cc_set_avg = np.average(cc_set, axis=1)
+    cc_set_std = np.std(cc_set, axis=1)
+    
+    # ----------------- Draw AC for x -----------------#
+    labels = ["AC(T)", "AC(F)", "AC(S)", "CC(F, S)"]
+    yl = [-0.8, 1.1]
+
+    for n in range(4):
+        # plt.subplot(1, 4, n+1)
+        plt.axes([0.1+0.22*n, 0.6, 0.15, 0.15])
+        plt.plot([0, 0], yl, 'g--', lw=1)
+        plt.plot([-0.1, 0.1], [0, 0], 'g--', lw=1)
+        plt.plot(tlag, cc_set_avg[n], c='k')
+        plt.fill_between(tlag, cc_set_avg[n]-cc_set_std[n]/2, cc_set_avg[n]+cc_set_std[n]/2, alpha=0.5, color='k', edgecolor="none")
+        plt.xlim([-0.1, 0.1])
+        plt.xlabel(r"$\Delta t$ (s)", fontsize=14)
+        plt.title(labels[n], fontsize=14)
+        plt.ylim(yl)
+        if n == 3:
+            plt.text(-0.06, -0.7, r"$V_F$ lead", horizontalalignment="center")
+            plt.text(0.06, -0.7, r"$V_S$ lead", horizontalalignment="center")
+        
+    # ----------------- Draw figure -----------------#
+    tags = ["T", "F", "S"]
+    xt = np.arange(0.5, 4.1, 0.5)
+    fl = [20, 80]
+    
+    for nid in range(3):
+
+        psd, ff, tf = hhsignal.get_stfft(data["vlfp"][nid], data["ts"], 2000, frange=[2, 100])
+        yf, f = hhsignal.get_fft(data["vlfp"][nid], 2000, frange=[2, 100])
+
+        plt.axes([0.1, 0.4-0.17*nid, 0.7, 0.15])
+        imshow_xy(psd, x=tf, y=ff, cmap="jet", interpolation="spline16")
+        plt.ylabel("frequency (%s) (Hz)"%(tags[nid]), fontsize=14)
+        plt.ylim(fl)
+        plt.colorbar()
+
+        if nid < 2:
+            plt.xticks(xt, labels=["" for _ in xt])
+        else:
+            plt.xticks(xt)
+            plt.xlabel("Time (s)", fontsize=14)
+
+        plt.axes([0.8, 0.4-0.17*nid, 0.11, 0.15])
+        yf_s = gaussian_filter1d(yf, 3)
+        plt.plot(yf, f, c='k')
+        plt.plot(yf_s, f, c='r', lw=1.5)
+        plt.xlabel(r"FFT($V_{%s}$)"%(tags[nid]), fontsize=14)
+        plt.ylim(fl)
+        
+    if fname is not None:
+        plt.savefig(fname, dpi=150)
+    
+    plt.show()
+    
